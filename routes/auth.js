f81fe2fdb258f29da706ae99cbb77a1b609da931
Abe <yourgmail@gmail.com>
2026-03-05 08:55:15 -0800

    fix register bug

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,21 @@
 router.post("/register", async (req, res) => {
 
-const { username, email, password } = req.body;
+const { Username, email, password } = req.body;
 
 const existingUser = await User.findOne({ email });
 if (existingUser) return res.json({ msg: "Email already exists" });
 
 const hashedPassword = await bcrypt.hash(password, 10);
 
-const user = new User({
-  username,
+const User = new User({
+  Username,
   email,
   password: hashedPassword
 });
 
 await user.save();
-
 const token = jwt.sign(
-  { id: user._id },
+  { id: User._id },
   process.env.JWT_SECRET,
   { expiresIn: "1h" }
 );
